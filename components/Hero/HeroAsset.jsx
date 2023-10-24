20394f75d4444748493299844dc88a878b9c9542
diovanne85 <dpsagosoy85@gmail.com>
2023-10-24 22:46:34 +0800

    adjust image texture

--- components/Hero/HeroAsset.jsx
+++ components/Hero/HeroAsset.jsx
@@ -1,78 +1,74 @@
-import React, { Suspense, useEffect, useRef } from "react";
-import { Canvas, render, useFrame } from "@react-three/fiber";
-import { OrbitControls, useGLTF} from "@react-three/drei";
+import * as THREE from 'three';
+import React, { Suspense} from "react";
+import { Canvas, render } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import styles from "./HeroAsset.module.css";
-
-
-
 
 
 export function Model(props) {
 
-  const { nodes, materials } = useGLTF("/boredApe-v4.glb");
-  return (
-    <group {...props} dispose={null}>
-      <group scale={0.05}>
-        <group
-          position={[11.69, 0, 5.867]}
-          rotation={[-Math.PI / 2, 0, -Math.PI]}
-          scale={100}
-        >
-          <mesh
-            geometry={nodes.Cube_Material_0.geometry}
-            material={materials.Material}
-          />
-          <mesh
-            geometry={nodes.Cube_Materiais001_0.geometry}
-            material={materials["Materiais.001"]}
-          />
-          <mesh
-            geometry={nodes.Cube_Materiais_0.geometry}
-            material={materials.Materiais}
-          />
-          <mesh
-            geometry={nodes.Cube_Materiais006_0.geometry}
-            material={materials["Materiais.006"]}
-          />
-          <mesh
-            geometry={nodes.Cube_Materiais005_0.geometry}
-            material={materials["Materiais.005"]}
-          />
-        </group>
-        <mesh
-          geometry={nodes.Texto_Materiais_0.geometry}
-          material={materials.Materiais}
-          position={[-0.101, -102.613, 1.475]}
-          rotation={[3.142, -1.57, 3.142]}
-          scale={6.678}
-        />
-        <mesh
-          geometry={nodes.Texto001_Material_0.geometry}
-          material={materials.Material}
-          position={[3.559, -116.758, 86.848]}
-          rotation={[3.142, -1.57, 3.142]}
-          scale={29.125}
-        />
-      </group>
-    </group>
-  );
+  const { nodes, materials } = useGLTF("/scene.gltf");
+   return (
+     <group {...props} dispose={null}>
+       <group scale={0.04}>
+         <group
+           position={[11.69, 0, 11.867]}
+           rotation={[-Math.PI / 2, 0, -Math.PI]}
+           scale={100}
+         >
+           <mesh
+             geometry={nodes.Cube_Material_0.geometry}
+             material={materials.Material}
+           />
+           <mesh
+             geometry={nodes.Cube_Materiais001_0.geometry}
+             material={materials["Materiais.001"]}
+           />
+           <mesh
+             geometry={nodes.Cube_Materiais_0.geometry}
+             material={materials.Materiais}
+           />
+           <mesh
+             geometry={nodes.Cube_Materiais006_0.geometry}
+             material={materials["Materiais.006"]}
+           />
+           <mesh
+             geometry={nodes.Cube_Materiais005_0.geometry}
+             material={materials["Materiais.005"]}
+           />
+         </group>
+         <mesh
+           geometry={nodes.Texto_Materiais_0.geometry}
+           material={materials.Materiais}
+           position={[-0.101, -102.613, 1.475]}
+           rotation={[3.142, -1.57, 3.142]}
+           scale={6.678}
+         />
+         <mesh
+           geometry={nodes.Texto001_Material_0.geometry}
+           material={materials.Material}
+           position={[3.559, -116.758, 86.848]}
+           rotation={[3.142, -1.57, 3.142]}
+           scale={29.125}
+         />
+       </group>
+     </group>
+   );
 }
 
 export function HeroAsset() {
-  
-
   return (
     <div className={styles.wrapper}>
-      <Canvas camera={{ fov: 100, position: [-10, 0, 0] }}>
+      <Canvas camera={{ fov: 100, position: [-7, 0, 0] }} >
         <Suspense fallback={null}>
-         
-          <ambientLight intensity={1}/>
+          <ambientLight intensity={1} position={[0.094, 1.725, 19.329]} />
           <directionalLight
-            castShadow={true}
             intensity={1}
-            position={[10, 0, 0]}
-            topLight={[10, 0, 0]}
+            position={[-10, 0, 0]}
+            pointLight={[10, 0, 0]}
           />
+          <pointLight position={[-0.993, 0.041, 0.145]} />
+          <hemisphereLight position={[0, 10, 0]} />
           <Model />
           <OrbitControls
             enablePan={true}
