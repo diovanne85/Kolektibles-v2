18ceaa758bfb83be319d840a8369c100e5efb7df
diovanne85 <dpsagosoy85@gmail.com>
2023-10-27 03:57:53 +0800

    adjust image to metallic

--- components/Hero/HeroAsset.jsx
+++ components/Hero/HeroAsset.jsx
@@ -1,12 +1,22 @@
-import * as THREE from "three";
-import React, { Suspense, useRef } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
+import { Canvas} from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import styles from "./HeroAsset.module.css";
+import * as THREE from 'three'
+
+
 
 export function Model(props) {
-  const { nodes, materials} = useGLTF("/boredApe.glb");
+  const { nodes, materials } = useGLTF("/bored.glb");
+  const textMaterial = new THREE.MeshBasicMaterial({ color: 0x29ffff });
+  const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x29ffff });
+  const customMaterial = new THREE.MeshStandardMaterial({
+    metalness: 1.5,
+    roughness: 0.2,
+    color: 0xffffff,
   
+  });
+ 
 
   return (
     <group {...props} dispose={null}>
@@ -15,11 +25,10 @@ export function Model(props) {
           position={[11.69, 0, 11.867]}
           rotation={[-Math.PI / 2, 0, -Math.PI]}
           scale={100}
-          roughness={0.2}
         >
           <mesh
             geometry={nodes.Cube_Material_0.geometry}
-            material={materials.Material}
+            material={customMaterial}
           />
           <mesh
             geometry={nodes.Cube_Materiais001_0.geometry}
@@ -27,7 +36,7 @@ export function Model(props) {
           />
           <mesh
             geometry={nodes.Cube_Materiais_0.geometry}
-            material={materials.Materiais}
+            material={cubeMaterial}
           />
           <mesh
             geometry={nodes.Cube_Materiais006_0.geometry}
@@ -40,7 +49,7 @@ export function Model(props) {
         </group>
         <mesh
           geometry={nodes.Texto_Materiais_0.geometry}
-          material={materials.Materiais}
+          material={textMaterial}
           position={[-0.101, -102.613, 1.475]}
           rotation={[3.142, -1.57, 3.142]}
           scale={6.678}
@@ -58,18 +67,23 @@ export function Model(props) {
 }
 
 export function HeroAsset() {
+ 
   return (
     <div className={styles.wrapper}>
-      <Canvas camera={{ fov: 60, position: [-7, 0, 0] }}>
+      <Canvas
+        camera={{ fov: 60, position: [-7, 0, 0] }}
+        
+      >
         <Suspense fallback={null}>
-          <ambientLight
-            intensity={2.5}
-            position={[0.96, -0.33, 0.112]}
-            scale={[1.706, 7.98, 12.821]}
+          <ambientLight intensity={0.8} position={[0.85, -0.035, -1.084]} />
+          <directionalLight
+            castShadow={true}
+            intensity={1}
+            position={[2.4, 2.29, -4.054]}
           />
-          <directionalLight castShadow={true} intensity={10} position={[2.095, 1.039, -1.031]} />
-          <pointLight intensity={15} position={[0.57, 1.058, -0.564]} />
+          <pointLight intensity={1} position={[-1.057, -0.664, 0]} />
           <Model />
+          <spotLight intensity={1} position={[5, 10, 7.5]} angle={0.314} />
           <OrbitControls
             enablePan={true}
             enableRotate={true}
