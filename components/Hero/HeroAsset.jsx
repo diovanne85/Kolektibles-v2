6ffff3a761639f11a24b55ed95513c34e64b6c97
diovanne85 <dpsagosoy85@gmail.com>
2023-10-27 15:02:22 +0800

    adjust image reflection

--- components/Hero/HeroAsset.jsx
+++ components/Hero/HeroAsset.jsx
@@ -1,22 +1,33 @@
 import React, { Suspense } from "react";
-import { Canvas} from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Reflector, useGLTF } from "@react-three/drei";
 import styles from "./HeroAsset.module.css";
-import * as THREE from 'three'
+import * as THREE from "three";
+
+
 
 
 
 export function Model(props) {
   const { nodes, materials } = useGLTF("/bored.glb");
-  const textMaterial = new THREE.MeshBasicMaterial({ color: 0x29ffff });
-  const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x29ffff });
-  const customMaterial = new THREE.MeshStandardMaterial({
-    metalness: 1.5,
-    roughness: 0.2,
-    color: 0xffffff,
-  
-  });
+
  
+  const textMaterial = new THREE.MeshStandardMaterial({ 
+    color: 0x29ffff,
+    reflectivity: 0.8,
+  });
+  const cubeMaterial = new THREE.MeshStandardMaterial({
+    color: 0x29ffff,
+    reflectivity: 0.5,
+  });
+  const customMaterial = new THREE.MeshPhysicalMaterial({
+    metalness: 0.5,
+    sheenRoughness: 0.2,
+    roughness: 0.1,
+    color: 0xffffff,
+    reflectivity: 0.2,
+  });
+
 
   return (
     <group {...props} dispose={null}>
@@ -67,23 +78,22 @@ export function Model(props) {
 }
 
 export function HeroAsset() {
- 
+
   return (
     <div className={styles.wrapper}>
-      <Canvas
-        camera={{ fov: 60, position: [-7, 0, 0] }}
-        
-      >
+      <Canvas camera={{ fov: 60, position: [-7, 0, 0] }}>
         <Suspense fallback={null}>
-          <ambientLight intensity={0.8} position={[0.85, -0.035, -1.084]} />
+          <ambientLight intensity={0.5} position={[0.85, -0.035, -1.084]} />
           <directionalLight
             castShadow={true}
-            intensity={1}
-            position={[2.4, 2.29, -4.054]}
+            intensity={2}
+            position={[2.4, 0.29, -6.054]}
           />
-          <pointLight intensity={1} position={[-1.057, -0.664, 0]} />
+          <pointLight intensity={5} position={[-0.75, 0.213, -0.039]} />
+
           <Model />
-          <spotLight intensity={1} position={[5, 10, 7.5]} angle={0.314} />
+          <pointLight intensity={4} position={[-1.057, -0.664, 0]} />
+          <spotLight intensity={5} position={[5, 10, 7.5]} angle={0.314} />
           <OrbitControls
             enablePan={true}
             enableRotate={true}
